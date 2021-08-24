41017c30fdbf0c755348c1eef73dd8bb5a376628
DouglasMacKrell <douglasmackrell@pursuit.org>
2021-08-23 21:55:11 -0400

    getLocation init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [latLong, setLatLong] = useState("");
+
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(response=>{
+      console.log(response.coords)
+      setLatLong(response.coords.latitude + "," + response.coords.longitude)
+    });
+  }
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
